ec790e9a75a4e3059a50d4ee14dc5b1ec54380a0
MD Tareq khan <102309745+Phychotk66@users.noreply.github.com>
2024-08-27 13:46:40 +0600

    Update settings.py

--- desimaster/settings.py
+++ desimaster/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-v4*tpzz3*srnosyhlnbyc-muo)lba3ba7&krpyqj%1#+r5vx8z'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
