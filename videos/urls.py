84d84aada7b220a92bd9c2739f57e4689a65249c
vendi <102309745+Phychotk66@users.noreply.github.com>
2024-08-31 16:12:32 +0600

    Update urls.py

--- videos/urls.py
+++ videos/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('video/<int:video_id>/', views.video_player, name='video_player'),
     path('category/<int:category_id>/', views.video_list, name='video_list'),
-    path('category/', views.video_list, {'category_id': None}, name='video_list_all'),  # Updated
+    path('category/<int:category_id>/subcategory/<int:subcategory_id>/', views.video_list, name='video_list'),
+    path('category/', views.video_list, {'category_id': None}, name='video_list_all'),
     path('upload/', views.upload_video, name='upload_video'),
 ]
