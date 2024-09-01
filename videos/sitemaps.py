e36c12cb116fcce54ff474f560d0db4b08ca1906
vendi <102309745+Phychotk66@users.noreply.github.com>
2024-09-01 17:03:02 +0600

    Update sitemaps.py

--- videos/sitemaps.py
+++ videos/sitemaps.py
@@ -12,15 +12,6 @@ class StaticViewSitemap(Sitemap):
     def location(self, item):
         return reverse(item)
 
-class VideoSitemap(Sitemap):
-    changefreq = "weekly"
-    priority = 0.7
-
-    def items(self):
-        return Video.objects.all()
-
-    def lastmod(self, obj):
-        return obj.created_at
 
 class CategorySitemap(Sitemap):
     changefreq = "weekly"
@@ -41,3 +32,27 @@ class SubCategorySitemap(Sitemap):
 
     def location(self, obj):
         return reverse('videos:video_list', args=[obj.category.id, obj.id])
+
+class VideoSitemap(Sitemap):
+    changefreq = "weekly"
+    priority = 0.7
+
+    def items(self):
+        return Video.objects.all()
+
+    def lastmod(self, obj):
+        return obj.created_at
+
+    def location(self, obj):
+        return obj.get_absolute_url()
+
+    def video_info(self, obj):
+        return {
+            'content_loc': obj.video_file.url,
+            'title': obj.title,
+            'description': obj.description,
+            'thumbnail_loc': obj.thumbnail.url if obj.thumbnail else None,
+            'player_loc': self.location(obj),
+            'duration': int(obj.duration.total_seconds()) if obj.duration else None,
+            'publication_date': obj.created_at.isoformat(),
+        }
